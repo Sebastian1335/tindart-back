9a8a37463bd416c2c0a448dc7dc11f24e515c840
Sebastián Guzmán <20200981@aloe.ulima.edu.pe>
2025-11-10 02:40:05 -0500

    fue p xd

--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -18,7 +18,6 @@ export class AuthMiddleware{
             
             const user = await prisma.user.findUnique({where: {id: +payload.id}})
             if (!user) return res.status(401).json({error: 'Invalid token - user'});
-            
             // if (!user.emailValidated) return res.status(401).json({error: 'Email invalido'});
             (req as any).user = UserEntity.fromObject(user)
             next()
